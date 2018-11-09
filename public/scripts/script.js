87dd0f18e42328bad476fd966f0c2ccfeeb10f92
Gauthier Jolly <gauthier.jolly@vonage.com>
2018-11-09 09:23:40 +0000

    Move scripts to /public and remove useless html file + add Makefile

---
+++ public/scripts/script.js
@@ -0,0 +1,44 @@
+const pollResult = async (uuid) => {
+    let url ='/result/' + uuid
+
+    try {
+        let response = await fetch(url)
+
+        let data = await response.json()
+        console.log(JSON.stringify(data))
+        document.getElementById("result").innerHTML = data.result
+    } catch(err) {
+        window.setTimeout(pollResult, 1000)
+        console.log(err)
+    }
+}
+
+const sendParameters = async () => {
+    let language = document.getElementById("language").value
+    let number = document.getElementById("destination").value
+
+    let body = {
+            language: language,
+            number: number
+        }
+    let url ='/asr'
+    let param = {
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        method: 'POST',
+        body: JSON.stringify(body)
+    }
+    console.log('backend url ' + url)
+
+    let response = await fetch(url, param)
+    let data = await response.json()
+    console.log(JSON.stringify(data))
+    pollResult(data.uuid)
+}
+
+const main = () => {
+    document.getElementById('sendButton').addEventListener("click", sendParameters)
+}
+
+window.addEventListener("load", main)
